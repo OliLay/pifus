b2b3a82fb7581df304eba3ca2d779552d6f7f360
Oliver Layer <olilay@live.de>
2022-03-05 21:03:29 +0100

    first experiment

--- api/benchmarks/writer/src/writer.c
+++ api/benchmarks/writer/src/writer.c
@@ -13,114 +13,149 @@
 #include "pifus_socket.h"
 #include "writer.h"
 
+char *write_filename;
+char *written_filename;
+volatile size_t total_dequeued;
+
 void print_result(struct pifus_operation_result *result) {
-  //printf("Result returned: opcode %s, result code %u \n",
-  //       operation_str(result->code), result->result_code);
+    // printf("Result returned: opcode %s, result code %u \n",
+    //        operation_str(result->code), result->result_code);
 }
 
 void parse_args(int argc, char *argv[], char **reader_ip, uint16_t *port,
                 enum pifus_priority *prio) {
-  if (argc < 3) {
-    printf("At least IP and port are required!\n");
-    exit(1);
-  }
-
-  int opt;
-  while ((opt = getopt(argc, argv, "i:p:l:")) != -1) {
-    switch (opt) {
-    case 'p':
-      *port = atoi(optarg);
-      break;
-    case 'l':
-      *prio = str_to_prio(optarg);
-      break;
-    default:
-      fprintf(stderr, "Usage: %s [-pl] [IP]\n", argv[0]);
-      exit(1);
+    if (argc < 3) {
+        printf("At least IP and port are required!\n");
+        exit(1);
     }
-  }
 
-  if (port == 0) {
-    printf("Port is required!\n");
-    exit(1);
-  }
+    int opt;
+    while ((opt = getopt(argc, argv, "i:p:l:")) != -1) {
+        switch (opt) {
+            case 'p':
+                *port = atoi(optarg);
+                break;
+            case 'l':
+                *prio = str_to_prio(optarg);
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-pl] [IP]\n", argv[0]);
+                exit(1);
+        }
+    }
 
-  *reader_ip = argv[optind];
+    if (port == 0) {
+        printf("Port is required!\n");
+        exit(1);
+    }
 
-  printf("Using reader IP '%s:%u' and prio '%s'\n", *reader_ip, *port,
-         prio_str(*prio));
+    if (!asprintf(&write_filename, "write%s.txt", prio_str(*prio))) {
+        exit(1);
+    }
+    if (!asprintf(&written_filename, "written%s.txt", prio_str(*prio))) {
+        exit(1);
+    }
+
+    *reader_ip = argv[optind];
+
+    printf("Using reader IP '%s:%u' and prio '%s'\n", *reader_ip, *port,
+           prio_str(*prio));
 }
 
 void write_csv(char *filename, long value) {
-  FILE *file = fopen(filename, "a");
-  fprintf(file, "%li\n", value);
-  fclose(file);
+    FILE *file = fopen(filename, "a");
+    fprintf(file, "%li\n", value);
+    fclose(file);
 }
 
+/** async version
 void callback_func(struct pifus_socket *socket,
                    enum pifus_operation_code op_code) {
   if (op_code == TCP_WRITE) {
     struct timeval tp;
     gettimeofday(&tp, NULL);
     long int us = tp.tv_sec * 1000000 + tp.tv_usec;
-    write_csv("written.csv", us);
+    write_csv(written_filename, us);
 
     struct pifus_operation_result result;
     pifus_socket_pop_result(socket, &result);
     print_result(&result);
   }
 }
+**/
+
+bool dequeue(struct pifus_socket *socket) {
+  struct pifus_operation_result result;
+    if (pifus_socket_pop_result(socket, &result)) {
+        struct timeval tp;
+        gettimeofday(&tp, NULL);
+        long int us = tp.tv_sec * 1000000 + tp.tv_usec;
+        write_csv(written_filename, us);
+        print_result(&result);
+        total_dequeued++;
+        return true;
+    }
+    return false;
+}
 
 int main(int argc, char *argv[]) {
-  printf("Starting pifus_writer...\n");
+    printf("Starting pifus_writer...\n");
 
-  char *reader_ip;
-  uint16_t port;
-  enum pifus_priority prio = PRIORITY_MEDIUM;
-  parse_args(argc, argv, &reader_ip, &port, &prio);
+    char *reader_ip;
+    uint16_t port;
+    enum pifus_priority prio = PRIORITY_MEDIUM;
+    parse_args(argc, argv, &reader_ip, &port, &prio);
 
-  pifus_initialize(&callback_func);
+    pifus_initialize(NULL);
 
-  struct pifus_socket *socket = pifus_socket(PROTOCOL_TCP, prio);
-  pifus_socket_bind(socket, PIFUS_IPV4_ADDR, 0);
+    struct pifus_socket *socket = pifus_socket(PROTOCOL_TCP, prio);
+    pifus_socket_bind(socket, PIFUS_IPV4_ADDR, 0);
 
-  struct pifus_operation_result operation_result;
-  pifus_socket_wait(socket, &operation_result);
-  print_result(&operation_result);
+    struct pifus_operation_result operation_result;
+    pifus_socket_wait(socket, &operation_result);
+    print_result(&operation_result);
 
-  struct pifus_ip_addr remote_addr;
-  ip_addr_from_string(reader_ip, &remote_addr);
-  pifus_socket_connect(socket, remote_addr, port);
-  pifus_socket_wait(socket, &operation_result);
-  print_result(&operation_result);
+    struct pifus_ip_addr remote_addr;
+    ip_addr_from_string(reader_ip, &remote_addr);
+    pifus_socket_connect(socket, remote_addr, port);
+    pifus_socket_wait(socket, &operation_result);
+    print_result(&operation_result);
 
-  size_t total_sent = 0;
-  struct timeval tp;
-  while (true) {
-    int sent = 0;
-    while (sent < 10) {
-      char *loop_data;
-      if (!asprintf(&loop_data,
-                    "Predictable interface for a user space IP stack!#%i",
-                    sent)) {
-        exit(1);
-      }
+    size_t total_sent = 0;
+    struct timeval tp;
+    while (true) {
+        int sent = 0;
+        while (sent < 10) {
+            char *loop_data;
+            if (!asprintf(&loop_data,
+                          "Predictable interface for a user space IP stack!#%i",
+                          sent)) {
+                exit(1);
+            }
 
-      gettimeofday(&tp, NULL);
-      if (pifus_socket_write(socket, loop_data, strlen(loop_data))) {
-        //printf("Wrote '%s'\n", loop_data);
-        sent++;
-        total_sent++;
-        long int us = tp.tv_sec * 1000000 + tp.tv_usec;
-        write_csv("write.csv", us);
-      }
+            while (total_dequeued + 50 < total_sent) {
+              dequeue(socket);
+            }
 
-      free(loop_data);
+            gettimeofday(&tp, NULL);
+            if (pifus_socket_write(socket, loop_data, strlen(loop_data))) {
+                // printf("Wrote '%s'\n", loop_data);
+                sent++;
+                total_sent++;
+                long int us = tp.tv_sec * 1000000 + tp.tv_usec;
+                write_csv(write_filename, us);
+            }
+
+            while (dequeue(socket)) {
+
+            }
+
+            free(loop_data);
+        }
+        // printf("Total sent: %lu\n", total_sent);
     }
-    //printf("Total sent: %lu\n", total_sent);
-  }
 
-  pifus_exit();
+    pifus_exit();
 
-  return 0;
+    return 0;
 }
--- benchmarks/priorities/competing.py
+++ benchmarks/priorities/competing.py
@@ -1,4 +1,6 @@
-import framework
+from framework import framework
+
+import statistics
 import time
 
 def start_measurement():
@@ -8,19 +10,27 @@ def start_measurement():
     lwip_reader_path = framework.get_binary_path(
         "lwip/custom/benchmark/reader/reader")
     framework.start_process(lwip_reader_path, args="192.168.1.201", tapif=1)
-   # framework.start_process(lwip_reader_path, args="192.168.1.202", tapif=1)
-    #framework.start_process(lwip_reader_path, args="192.168.1.203", tapif=1)
+    framework.start_process(lwip_reader_path, args="192.168.1.202", tapif=2)
+    framework.start_process(lwip_reader_path, args="192.168.1.203", tapif=3)
+    
+    # make sure readers are up
+    time.sleep(1)
 
     pifus_writer_path = framework.get_binary_path(
         "api/benchmarks/writer/pifus_writer")
     framework.start_process(pifus_writer_path, args="192.168.1.201 -p 11337 -l HIGH")
-    #framework.start_process(pifus_writer_path, args="192.168.1.202 -p 11337 -l MEDIUM")
-    #framework.start_process(pifus_writer_path, args="192.168.1.203 -p 11337 -l LOW")
+    framework.start_process(pifus_writer_path, args="192.168.1.202 -p 11337 -l MEDIUM")
+    framework.start_process(pifus_writer_path, args="192.168.1.203 -p 11337 -l LOW")
 
-    time.sleep(3)
+    time.sleep(20)
     framework.kill_all_processes()
 
-    print(framework.compute_latency_from_stamps("write.csv", "written.csv"))
+    print ("HIGH")
+    print(statistics.mean(framework.compute_latency_from_stamps("writePRIORITY_HIGH.txt", "writtenPRIORITY_HIGH.txt")))
+    print ("MEDIUM")
+    print(statistics.mean(framework.compute_latency_from_stamps("writePRIORITY_MEDIUM.txt", "writtenPRIORITY_MEDIUM.txt")))
+    print ("LOW")
+    print(statistics.mean(framework.compute_latency_from_stamps("writePRIORITY_LOW.txt", "writtenPRIORITY_LOW.txt")))
 
     # TODO: parse write and written and compute latency and check if legit
 
